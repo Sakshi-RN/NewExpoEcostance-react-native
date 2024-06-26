3dc790f57384131371e709629de3ea37734938a2
AltairFarooque24 <166220493+AltairFarooque24@users.noreply.github.com>
2024-06-26 15:29:12 +0530

    added and updated the fonts

--- App.js
+++ App.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { View, StyleSheet } from "react-native";
 import { Provider } from "react-redux";
 import Navigation from "./src/routes/navigation";
 import store from "./src/redux/storeConfig";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    "WorkSans-Medium": require("./src/assets/fonts/WorkSans-Medium.ttf"),
+    "WorkSans-Regular": require("./src/assets/fonts/WorkSans-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <Provider store={store}>
-      <Navigation />
-    </Provider>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Provider store={store}>
+        <Navigation />
+      </Provider>
+    </View>
   );
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
--- src/theme/fonts.js
+++ src/theme/fonts.js
@@ -1,10 +1,4 @@
-// import * as Font from "expo-font";
-
-// const fetchFonts = () => {
-//   return Font.loadAsync({
-//     "WorkSans-Regular": require("../assets/fonts/WorkSans-Regular.ttf"),
-//     "WorkSans-Medium": require("../assets/fonts/WorkSans-Medium.ttf"),
-//   });
-// };
-
-// export { fetchFonts };
+export class Fonts {
+    static regular = "WorkSans-Regular";
+    static medium = "WorkSans-Medium";
+}
