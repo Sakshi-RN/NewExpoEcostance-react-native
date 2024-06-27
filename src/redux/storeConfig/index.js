e29410aca97ff8a0133f054cc3c0fad39801c1fc
AltairFarooque24 <166220493+AltairFarooque24@users.noreply.github.com>
2024-06-27 14:43:26 +0530

    merge with main code

--- src/redux/storeConfig/index.js
+++ src/redux/storeConfig/index.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import { combineReducers } from "redux";
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../features/authReducer";
-import countryCodeReducer from "../features/countryCodeReducer";
-import { productReducer } from "../features/getetProductReducer/index";
-import userReducer from "../features/userReducer";
-import carbonReducer from "../features/carbonReducer";
-import cartReducer from "../features/cartReducer";
-import profileReducer from "../features/profileReducer";
-
-const reducer = combineReducers({
-  auth: authReducer,
-  country: countryCodeReducer,
-  product: productReducer,
-  user: userReducer,
-  carbon: carbonReducer,
-  cart: cartReducer,
-  profile: profileReducer,
-});
-=======
 
 import { combineReducers } from 'redux'
 import {configureStore} from '@reduxjs/toolkit'
@@ -44,7 +23,6 @@ const reducer = combineReducers({
     address:ChangeAddressFunc 
 })
 
->>>>>>> e01a94cc4bc975358aedbd4fba825b3b5eb03e65
 
 const store = configureStore({
   reducer,
